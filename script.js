95d0510250c5fc794533dbac1b9a80326e5a312e
AlanMML <98406860+AlanMML@users.noreply.github.com>
2022-06-22 12:01:27 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,78 +1,79 @@
 // funções
 function pulalinha() {
-    
-    document.write("<br>");
-    
-};
 
-function mostra(frase) {
-    
-    document.write(frase)
-    
-    pulalinha()
+    document.write("<br>")
 
-    }
-
-function mostraconsole(frase) {
-    
-    console.log(frase)
-    
 }
 
-// var's
+function mostra(frase) {
 
-var segredos = [16, 34, 37, 42, 50, 58];
-segredos[0] = 20;
-console.log(segredos[0]);
-segredos.push(60);
-console.log(segredos[6]);
-console.log(segredos.length);
+    document.write(frase)
 
-sorteados = []
-var adicionarSorteado = function() {
-var numero = document.getElementById("numero").value;
-sorteados.push(numero);
-console.log(sorteados);
+    pulalinha()
 
+}
 
+function mostraconsole(frase) {
 
-var adicionarSorteado = function() {
+    console.log(frase)
+}
+
+var sorteados = [];
+
+var adicionarSorteado = function () {
     var numero = document.getElementById("numero").value;
-    for(var i = 0; i < sorteados.length; i = i + 1) {
-        
-        if(sorteados[i] == numero) {
+    for (var i = 0; i < 6; i = i + 1) {
+        if (sorteados[i] == numero) {
             alert("Numero já sorteado!");
             return;
-        };
+        }
+    }
+    sorteados.push(numero);
+    console.log(sorteados);
 };
-};
+
 var botao = document.getElementById("verificar");
 botao.onclick = adicionarSorteado;
-sorteados.push(numero);
-console.log(sorteados);
+
+var caixaDoNumero = document.getElementById("numero");
 
 
-    
-var caixaDoNumero = document.getElementById("numero"); 
+segredos = [];
 
-var botaoClicado = function() {
-    
-    for(var i = 0; i < segredos.length; i = i + 1) {
-        
-        if(segredos[i] == caixaDoNumero.value) {
-            
+for (var i = 0; i < 6; i += 1) {
+    segredos.push(Math.round(Math.random() * 10));
+}
+
+var botaoClicado = function () {
+
+    for (var i = 0; i < segredos.length; i = i + 1) {
+
+        if (segredos[i] == caixaDoNumero.value) {
+
             alert("Parabéns! Você acertou um dos números secretos");
-            caixaDoNumero.value= "";
+            caixaDoNumero.value = "";
             return;
-            
+
         }
     }
     alert("Que pena, você errou")
-    caixaDoNumero.value= "";
+    caixaDoNumero.value = "";
 };
 
-var botaoAdivinhar = document.getElementById("adivinhar");
+chute = [];
+for (var i = 0; i < 6; i++) {
+    chute.push(parseInt(prompt('digite um numero de 1 a 10')))
+}
 
-botaoAdivinhar.onclick = botaoClicado;
+var acertos = 0;
 
+for (var i = 0; i < 6; i++) {
+    for (var o = 0; o < 6; o++)
+        if (chute[i] === segredos[o]) {
+            acertos = acertos + 1
+        };
+};
 
+mostraconsole(acertos)
+mostraconsole(chute)
+mostraconsole(segredos)
